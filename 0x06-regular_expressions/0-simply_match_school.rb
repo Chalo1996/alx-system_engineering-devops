d0b2b6db3e4dd29af5e6e5e58ddc739fe7809ca2
Chalo1996 <emusyoka759@gmail.com>
2022-08-23 11:56:23 +0000

    Matches School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -3,4 +3,4 @@
 #Using the project instructions, create a Ruby script that accepts
 #one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/S\w{4}l/).join
+puts ARGV[0].scan(/Scho{2}l/).join
